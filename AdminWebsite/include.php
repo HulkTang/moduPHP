4fcb6eb7c972fdb52687fc77eb0d3be2b5e821a8
qiancs1101 <qian.cs@outlook.com>
2016-12-12 18:48:16 +0800

    update

--- AdminWebsite/include.php
+++ AdminWebsite/include.php
@@ -13,7 +13,6 @@ require_once "core/order.inc.php";
 require_once "core/customer.inc.php";
 require_once "core/others.inc.php";
 require_once "core/HttpClient.class.php";
-require_once "core/PrinterAPI.php";
 require_once "lib/page.func.php";
 require_once "lib/upload.func.php";
 require_once "configs/parameters.php";
